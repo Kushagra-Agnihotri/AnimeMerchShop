12a04dae293d1592f3dd0331007f234c3e7155e5
Kushagra Agnihotri <vagnikush@gmail.com>
2025-09-24 19:19:44 +0530

    Add signup and login serializers

---
+++ users/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth import authenticate
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
